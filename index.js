c91711d00368792dae065b102950a871c0b3bdcc
JunayetTamim <junayettamim186@gmail.com>
2022-05-16 21:56:49 +0600

    mongodb add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,35 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+//MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oytlp.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+        await client.connect();
+        const serviceCollection = client.db("doctors_care").collection("services");
+
+        app.get('/service', async (req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+
+    }
+    finally{
+
+    }
+
+}
+run().catch(console.dir);
+
+
+//Get Data
 app.get('/', (req, res) => {
     res.send('Hello From Doctors Care')
 })
