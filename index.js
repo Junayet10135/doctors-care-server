afda9efdb4bd2bfc2b7c6640182be5ffe085c51b
JunayetTamim <junayettamim186@gmail.com>
2022-05-18 23:26:42 +0600

    use put for recive user

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run(){
         await client.connect();
         const serviceCollection = client.db("doctors_care").collection("services");
         const bookingCollection = client.db("doctors_care").collection("bookings");
+        const userCollection = client.db("doctors_care").collection("users");
 
         app.get('/service', async (req, res)=>{
             const query = {};
@@ -28,6 +29,18 @@ async function run(){
             res.send(services);
 
         });
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
 
         // Warning: This is not the proper way to query multiple collection. 
         // After learning more about mongodb. use aggregate, lookup, pipeline, match, group
@@ -55,6 +68,13 @@ async function run(){
 
 
             res.send(services);
+        })
+
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
         })
 
         app.post('/booking', async (req, res)=>{
